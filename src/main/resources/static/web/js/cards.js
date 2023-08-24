8040d0ce4849a55db1b1015d7ede9d3e09d41c54
Rey <reinielgarcia96@gmail.com>
2023-08-24 17:18:23 -0300

    Correcciones: Agrego del Static tarea 7.

--- src/main/resources/static/web/js/cards.js
+++ src/main/resources/static/web/js/cards.js
@@ -1,4 +1,5 @@
 Vue.createApp({
+
     data() {
         return {
             clientInfo: {},
@@ -10,7 +11,7 @@ Vue.createApp({
     },
     methods: {
         getData: function () {
-            axios.get("/api/clients/1")
+            axios.get("/api/clients/current")
                 .then((response) => {
                     //get client ifo
                     this.clientInfo = response.data;
@@ -24,7 +25,15 @@ Vue.createApp({
         },
         formatDate: function (date) {
             return new Date(date).toLocaleDateString('en-gb');
-        }
+        },
+        signOut: function () {
+            axios.post('/api/logout')
+                .then(response => window.location.href = "/web/index.html")
+                .catch(() => {
+                    this.errorMsg = "Sign out failed"
+                    this.errorToats.show();
+                })
+        },
     },
     mounted: function () {
         this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
